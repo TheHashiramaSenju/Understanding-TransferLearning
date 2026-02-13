261c2ef611dc1497b6d17e9184e525b709e73cbb
TheHashiramaSenju <darsh.astro@outlook.com>
2026-02-13 11:27:18 +0530

    Added the 1.3.1 ALR module

--- star1/star1.3.1trainmodel.py
+++ star1/star1.3.1trainmodel.py
@@ -1,1 +1,20 @@
 #here we will watch how we warmup the learning rates of the model that we want to train !
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+from tensorflow.keras.preprocesssing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+from tensorflow.keras.applications import MobileNetV2
+import matplotlib.pyplot as plt
+import os
+
+image_size = (224, 224)
+batch_size = 32
+epochs = 10
+learning_rate = 0.0001
+layers_unfreeze = 80
+initial_learning_rate = 0.0001
+
+
+
+
